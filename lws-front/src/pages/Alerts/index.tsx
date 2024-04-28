05cbe979523a6649a8bf757ed121212f275ee1b1
caio <caiovitordias1@gmail.com>
2024-04-28 19:27:49 -0300

    alerts/pages- inicio da estrutura da criação de alertas

--- lws-front/src/pages/Alerts/index.tsx
+++ lws-front/src/pages/Alerts/index.tsx
@@ -2,7 +2,31 @@ import "./style.scss"
 
 export default function Alerts() {
   return (
-    <></>
+    <>
+      <div className="corpoAlerta">
+        <div>
+          <h1>Criar Alerta</h1>
+        </div>
+        <form className="corpoFormulario">
+          <div className="">
+            <label></label>
+            <input></input>
+          </div>
+          <div>
+            <label></label>
+            <input></input>
+          </div>
+          <div>
+            <label></label>
+            <input></input>
+          </div>
+          <div>
+            <label></label>
+            <input></input>
+          </div>
+        </form>
+      </div>
+    </>
   )
 }
 
