2534277507777b1b8115d15d9d1c980f9b0b3e56
caio <caiovitordias1@gmail.com>
2024-04-29 08:39:32 -0300

    pages/alerts - criacao da funcao de modificar componente

--- lws-front/src/pages/Alerts/index.tsx
+++ lws-front/src/pages/Alerts/index.tsx
@@ -1,16 +1,35 @@
+import { useState } from "react"
 import "./style.scss"
 
 export default function Alerts() {
+
+  const [dadosAlerta, setDadosAlerta] = useState({
+    condicao: ""
+  })
+
+  const handleChange = (enviar: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement>) => {
+    const {name, value} = enviar.target;
+    setDadosAlerta(prevState => (
+      {...prevState, 
+        [name]: value
+      }
+    ))
+  }
+
+  const handleSubmit = () =>{
+
+  }
+
   return (
     <>
       <div className="corpoAlerta">
         <div>
           <h1>Crie um Alerta</h1>
         </div>
-        <form className="corpoFormulario">
+        <form className="corpoFormulario" onSubmit={handleSubmit}>
           <div className="campoCondicao">
             <label>Condição</label>
-            <select>
+            <select value={dadosAlerta.condicao} onChange={handleChange}>
               <option value="igual">igual</option>
               <option value="maior que">maior que</option>
               <option value="menor que">menor que</option>
@@ -24,12 +43,17 @@ export default function Alerts() {
           </div>
           <div>
             <label>Estação</label>
-            <input type="string"></input>
+            <select>
+              <option>estação 1</option>
+              <option>estação 2</option>
+            </select>
           </div>
           <div>
             <label>Ocorrência</label>
             <input type="string"></input>
           </div>
+
+          <button type="submit">Criar</button>
         </form>
       </div>
     </>
